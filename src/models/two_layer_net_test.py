65e3c8275d2020aa5e499d1e7292e2aadde71c6a
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2025-11-08 22:19:05 +0900

    feat: two_layer_net_test.py 추가 train acc: 0.8895 test acc: 0.8673 교재 5장에 나온 부분을 주어진 데이터와 연결 후 테스트 완료. 터미널에서 폴더 경로 지정 후 python명령어로 실행 시 정상작동

---
+++ src/models/two_layer_net_test.py
@@ -0,0 +1,53 @@
+import sys,os
+import numpy as np
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from data.mnist_reader import load_mnist
+from common.functions import to_one_hot
+from two_layer_net import TwoLayerNet
+
+# 학습(train) 데이터 불러오기
+x_train, t_train = load_mnist('../data', kind='train')
+
+# 테스트(test) 데이터 불러오기
+x_test, t_test = load_mnist('../data', kind='t10k')
+
+# 값 정규화 
+x_train = x_train.astype(np.float32) / 255.0
+x_test = x_test.astype(np.float32) / 255.0
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+
+iters_num = 10000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+iter_per_epoch = max(train_size / batch_size, 1)
+
+for i in range(iters_num):
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = to_one_hot(t_train[batch_mask]) # 원 핫 인코딩 필요
+    
+    grad = network.gradient(x_batch, t_batch)
+    
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+        
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+    
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        
+        print(f"train acc: {train_acc}\ntest acc: {test_acc}")
