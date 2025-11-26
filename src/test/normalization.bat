f48abb57f11765406de26236e26aad90a26ae2fd
mhe1239 <107838341+mhe1239@users.noreply.github.com>
2025-11-26 12:57:13 +0900

    refactor:  전체 탐색(.py파일) 파일 출력 경로를 target 폴더로 통일

--- src/test/normalization.bat
+++ src/test/normalization.bat
@@ -1,2 +1,4 @@
-@REM output_log.txt에서 normalization.txt으로 변경
-python hyperparameter_tuning_loss.py > normalization.txt
+@REM output_log.txt에서 ..\target\normalization.txt으로 변경
+@REM 출력을 리다이렉션할 때 파일이 없다면 자동으로 생성한다.
+@REM 출력 경로를 src\target 폴더로 변경시에 ..\target\name.txt으로 변경
+python hyperparameter_tuning_loss.py > ..\target\normalization.txt
--- src/test/sgd.bat
+++ src/test/sgd.bat
@@ -1,2 +1,4 @@
-@REM output_log.txt에서 sgd.txt으로 변경
-python hyperparameter_tuning_loss.py > sgd.txt
+@REM output_log.txt에서 ..\target\sgd.txt으로 변경
+@REM 출력을 리다이렉션할 때 파일이 없다면 자동으로 생성한다.
+@REM 출력 경로를 src\target 폴더로 변경시에 ..\target\name.txt으로 변경
+python hyperparameter_tuning_loss.py > ..\target\sgd.txt
