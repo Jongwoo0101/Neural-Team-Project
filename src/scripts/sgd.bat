58717fa9e1fc2354cda92b7631f65ba0c4d3d85d
mhe1239 <107838341+mhe1239@users.noreply.github.com>
2025-11-29 14:30:22 +0900

    chore: hyperparameter_tuning파일 이름에 따른 변경

--- src/scripts/sgd.bat
+++ src/scripts/sgd.bat
@@ -2,5 +2,5 @@
 @REM 출력을 리다이렉션할 때 파일이 없다면 자동으로 생성한다.
 @REM 1. 실행할 스크립트(.py)의 경로: 상위 폴더(src)에서 test 폴더로 이동하여 실행
 @REM 2. 출력 파일(.txt)의 경로: 상위 폴더(src)에서 target 폴더로 이동하여 저장
-..\test\hyperparameter_tuning_loss.py > ..\target\loss_acc_log.txt
-python hyperparameter_tuning_loss.py > ..\target\sgd.txt
+..\test\hyperparameter_tuning.py > ..\target\loss_acc_log.txt
+python hyperparameter_tuning.py > ..\target\sgd.txt
